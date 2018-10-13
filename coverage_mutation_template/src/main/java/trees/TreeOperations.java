240aa00c14c7bf2b766e9d28dbd1a2c943ee7aa7
Michael Munje <michael.munje.919@my.csun.edu>
2018-10-13 12:19:16 -0700

    Added BFS method for the tree.

--- coverage_mutation_template/src/main/java/trees/TreeOperations.java
+++ coverage_mutation_template/src/main/java/trees/TreeOperations.java
@@ -4,4 +4,40 @@ import java.util.LinkedList;
 
 public class TreeOperations {
     // TODO: write any needed code here
+    Boolean breadthfirstSearch(Node target, Node head)
+    {
+    	Queue<Node> nodeQueue = new LinkedList<> (); 
+    	if (head == null)
+    		return false;
+
+    	if (head.left != null)
+    	{
+    		nodeQueue.add(head.left);
+    	}
+
+    	if (head.right != null)
+    	{
+    		nodeQueue.add(head.right);
+    	}
+
+    	while (!nodeQueue.isEmpty())
+    	{
+    		Node newNode = nodeQueue.remove();
+
+    		if (newNode.contents == target.contents)
+    			return true;
+
+	    	if (newNode.left != null)
+	    	{
+	    		nodeQueue.add(head.left);
+	    	}
+
+	    	if (newNode.right != null)
+	    	{
+	    		nodeQueue.add(head.right);
+	    	}
+    	};
+
+    	return false;
+    }
 }
