1acfc1d91b4c2e7eeb89a8349387bf19c2a8bea2
Michael Munje <michael.munje.919@my.csun.edu>
2018-10-13 13:38:22 -0700

    Added more content to the basic unit test.

--- coverage_mutation_template/src/test/java/trees/TreeOperationsTest.java
+++ coverage_mutation_template/src/test/java/trees/TreeOperationsTest.java
@@ -9,8 +9,15 @@ public class TreeOperationsTest {
         // illustrates how to write a test
         // assertEquals(expected, expression_being_tested)
         assertEquals(2, 1 + 1);
+
         Node rootNode = new Node(5,null,null);
         assertEquals(0,TreeOperations.maxDepth(rootNode));
+
+        Node newNode = new Node(10,rootNode,null);
+        // now we have a tree combines the previous tree.
+        // max depth should be 1.
+        // contents of each node should be 5 and 10.
+        assertEquals(1,TreeOperations.maxDepth(newNode));
     
     }
 }
